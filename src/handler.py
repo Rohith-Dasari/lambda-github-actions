47b9583493d4c8369ad74420a1db61035899661e
rohithdasariformal@gmail.com <rohithdasariformal@gmail.com>
2026-02-02 15:38:13 +0530

    added newman testing

--- src/handler.py
+++ src/handler.py
@@ -2,5 +2,6 @@ def lambda_handler(event, context):
     name = event.get("name", "World")
     return {
         "statusCode": 200,
-        "body": f"Hello, {name}!"
+        "headers": {"Content-Type": "text/plain"},
+        "body": f"Hello, {name}!",
     }
